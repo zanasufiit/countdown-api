2a1de2f95ac3809f18a8ff078f054dc819aae093
kamko <j@necek.com>
2020-03-07 21:32:25 +0100

    load value from znf.sk

--- countdown-api.go
+++ countdown-api.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"github.com/tidwall/gjson"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"regexp"
 	"strconv"
+	"time"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
@@ -11,14 +15,14 @@ import (
 
 type Students struct {
 	Count int `json:"count"`
-  }
+}
 
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 
 	e.Any("/*", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, Students { Count: 250 })
+		return c.JSON(http.StatusOK, Students{Count: studentsCount()})
 	})
 
 	e.Logger.Fatal(e.Start(":" + port()))
@@ -36,4 +40,35 @@ func port() (port string) {
 	}
 
 	return
+}
+
+var httpClient = http.Client{
+	Timeout: time.Second * 5,
+}
+
+var rgx = regexp.MustCompile(`Aktuálny počet podpisov: (\d+)`)
+
+func studentsCount() int {
+	res, err := httpClient.Get("https://www.zanasufiit.sk/wp-json/wp/v2/posts/120")
+	if err != nil {
+		return -1
+	}
+
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return -1
+	}
+	bodyBytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return -1
+	}
+	text := gjson.Get(string(bodyBytes), "excerpt.rendered")
+
+	rs := rgx.FindStringSubmatch(text.String())
+	count, err := strconv.Atoi(rs[1])
+	if err != nil {
+		return -1
+	}
+
+	return count
 }
--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/tidwall/gjson v1.6.0
 	golang.org/x/crypto v0.0.0-20191219195013-becbf705a915 // indirect
 )
